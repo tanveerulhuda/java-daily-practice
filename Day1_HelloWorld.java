83dbd51bb175a4add178a8a49d0262b50dfa4293
tanveerilhuda70@gmail.com <tanveerulhuda70@gmail.com>
2025-11-22 23:19:46 +0530

    Day 1 to Day 7 Java Basics added

---
+++ Day1_HelloWorld.java
@@ -0,0 +1,8 @@
+public class Day1_HelloWorld {
+    public static void main(String[] args) {
+        String name = "Tanveer";
+        int age = 23;
+        System.out.println("Hello, World!");
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
