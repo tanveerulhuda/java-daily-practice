7fd7ca4e9876c45c4bebf72093407fdb7fb6add7
tanveerilhuda70@gmail.com <tanveerulhuda70@gmail.com>
2026-01-22 23:07:09 +0530

    Day 15 Calculator

---
+++ Day15_Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Day15_Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Choose operation (+ - * /): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("Result: " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+
+            case '/':
+                System.out.println("Result: " + (a / b));
+                break;
+
+            default:
+                System.out.println("Invalid operation");
+        }
+
+        sc.close();
+    }
+}
